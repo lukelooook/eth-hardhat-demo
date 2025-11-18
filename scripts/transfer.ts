22a00037b20c1278b8fa6d6f1329f2f0e84f6ed9
luke.sha <suxsyn@gmail.com>
2025-11-18 22:56:08 +0800

    fix

--- scripts/transfer.ts
+++ scripts/transfer.ts
@@ -8,7 +8,7 @@ async function main() {
   const [sender, receiver] = await viem.getWalletClients();
   const publicClient = await viem.getPublicClient();
 
-  const tokenAddress = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"; // 替换为实际合约地址
+  const tokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // 替换为实际合约地址
 
   // 直接获取合约实例，不指定 client（使用默认客户端）
   const token = await viem.getContractAt("FirstToken", tokenAddress);
